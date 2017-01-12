ed9e0719a7afca844a6c7ccba66b7ce4ddaefc2c
Student 6 <Student 6@10.8.33.152>
2017-01-12 12:15:38 -0500

    Stuff

--- SimonDavidM/src/simonDavidM/SimonScreenDavid.java
+++ SimonDavidM/src/simonDavidM/SimonScreenDavid.java
@@ -93,6 +93,7 @@ public class SimonScreenDavid extends ClickableScreen implements Runnable {
 		}
 		lastMove = random;
 		return getMove(validMoves[random]);
+		//
 	}
 
 	public MoveInterfaceDavid getMove(ButtonInterfaceDavid newMove) {
